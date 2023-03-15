e62b6d795c0374bf330ddfee5cee40046d53929d
Destman88 <derickmaitland@gmail.com>
2023-03-14 21:34:23 -0600

    interfaz de calculator app

---
+++ 1_Nivel_Principiante/CalculatorApp/calculator-app/src/components/Calculator.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Board from "./Board";
+import styles from "../css/Calculator.module.css";
+import Display from "./Display";
+
+const Calculator = () => {
+  return (
+    <div className={styles.calculator}>
+      <Display />
+      <Board />
+    </div>
+  );
+};
+
+export default Calculator;
